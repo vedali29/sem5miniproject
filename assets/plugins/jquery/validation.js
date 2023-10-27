2c341a6e79839b4531f8b4ef563b995b94d65445
vedali29 <vedalipawar29@gmail.com>
2023-10-28 00:53:31 +0530

    here i'm

--- assets/plugins/jquery/validation.js
+++ assets/plugins/jquery/validation.js
@@ -2,9 +2,9 @@
 function userlogin(form)/*function to check userid & password*/
 {
  /*the following code checkes whether the entered userid and password are matching*/
- if(form.userid.value == "nirav@gmail.com" && form.usrpsw.value == "123")
+ if(form.userid.value == "vedali@gmail.com" && form.usrpsw.value == "123")
   {
-    window.open('index.html')/*opens the target page while Id & password matches*/
+    window.open('profile.html')/*opens the target page while Id & password matches*/
   }
  else
  {
